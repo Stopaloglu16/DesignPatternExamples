075f61859e544e9404423f91d1aaf16170e759de
Sertac Topaloglu <sertac.topaloglu@hotmail.co.uk>
2022-08-23 19:31:51 +0100

    Abstract

---
+++ DesignPatterEntity.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternExamples
+{
+    public class DesignPatterEntity
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? TypeName { get; set; }
+
+        public string? Description { get; set; }
+
+    }
+
+    public class DesignPatterList
+    {
+
+        public static List<DesignPatterEntity> GetList()
+        {
+            int index = 1;
+
+            List<DesignPatterEntity> myList = new List<DesignPatterEntity>() {
+                    new DesignPatterEntity() {TypeName = "Creational Patterns",  Id = index, Name = "Abstract Method"}, //1
+                    new DesignPatterEntity() {TypeName = "Creational Patterns", Id = ++index, Name = "Builder"},
+                    new DesignPatterEntity() {TypeName = "Creational Patterns", Id = ++index, Name = "Factory Method"},
+                    new DesignPatterEntity() {TypeName = "Creational Patterns", Id = ++index, Name = "Prototype"},
+                    new DesignPatterEntity() {TypeName = "Creational Patterns", Id = ++index, Name = "Singleton"},
+
+                    new DesignPatterEntity() {TypeName = "Structural Patterns", Id = ++index, Name = "Adapter"},//6
+                    new DesignPatterEntity() {TypeName = "Structural Patterns", Id = ++index, Name = "Bridge"},
+                    new DesignPatterEntity() {TypeName = "Structural Patterns", Id = ++index, Name = "Composite"},//8
+                    new DesignPatterEntity() {TypeName = "Structural Patterns", Id = ++index, Name = "Decorator"}, //9
+                    new DesignPatterEntity() {TypeName = "Structural Patterns", Id = ++index, Name = "Facade"},
+                    new DesignPatterEntity() {TypeName = "Structural Patterns", Id = ++index, Name = "Flyweight"},
+                    new DesignPatterEntity() {TypeName = "Structural Patterns", Id = ++index, Name = "Proxy"},
+
+                    new DesignPatterEntity() {TypeName = "Behavioral Patterns", Id = ++index, Name = "Command"},
+                    new DesignPatterEntity() {TypeName = "Behavioral Patterns", Id = ++index, Name = "Interpreter"},
+                    new DesignPatterEntity() {TypeName = "Behavioral Patterns", Id = ++index, Name = "Iterator"},
+                    new DesignPatterEntity() {TypeName = "Behavioral Patterns", Id = ++index, Name = "Mediator"},
+                    new DesignPatterEntity() {TypeName = "Behavioral Patterns", Id = ++index, Name = "Memento"},
+                    new DesignPatterEntity() {TypeName = "Behavioral Patterns", Id = ++index, Name = "Observer"},
+                    new DesignPatterEntity() {TypeName = "Behavioral Patterns", Id = ++index, Name = "State"},
+                    new DesignPatterEntity() {TypeName = "Behavioral Patterns", Id = ++index, Name = "Strategy"},
+                    new DesignPatterEntity() {TypeName = "Behavioral Patterns", Id = ++index, Name = "Template"},
+                    new DesignPatterEntity() {TypeName = "Behavioral Patterns", Id = ++index, Name = "Visitor"}
+
+            };
+
+            return myList;
+        }
+
+
+        public static void WriteList(List<DesignPatterEntity> myPatterns)
+        {
+            foreach (var myPattern in myPatterns)
+            {
+                Console.WriteLine(myPattern.Id + " " + myPattern.Name);
+            }
+        }
+
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,71 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using DesignPatternExamples.CreationalPatterns;
+using DesignPatternExamples.StructuralPatterns;
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Welcome desing patters");
+
+var myPatterns = DesignPatternExamples.DesignPatterList.GetList();
 
 
-FactoryMethodCreate.Create();
+DesignPatternExamples.DesignPatterList.WriteList(myPatterns);
 
-// Wait for user
-Console.ReadKey();
+
+while (true)
+{
+    
+    var myEntry = Console.ReadLine();
+
+    if (int.TryParse(myEntry, out int value))
+    {
+
+        switch (Convert.ToInt32(myEntry))
+        {
+            case 1:
+                AbstractFactoryCreate.Create();
+                break;
+
+            case 3:
+                FactoryMethodCreate.Create();
+                break;
+
+
+            case 6:
+                AdapterSample.Create();
+                break;
+
+            case 8:
+                CompositeSample.Create();
+                break;
+
+            case 9:
+                DecoratorSample.Create();
+                break;
+
+
+
+            case 0:
+                break;
+
+            default:
+                Console.WriteLine("unknown");
+                break;
+        }
+
+        if (Convert.ToInt32(myEntry) == 0) break;
+
+    }
+    else
+    {
+        Console.Clear();
+
+        DesignPatternExamples.DesignPatterList.WriteList(myPatterns);
+
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("===============Pls choose your design!!!===============");
+        Console.ForegroundColor = ConsoleColor.White;
+        
+    }
+}
+
+
+
